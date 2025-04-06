d32927df2162b84af07e523948f47c7a080a82f7
didot31 <dba.sutherland@gmail.com>
2025-04-06 11:50:24 -0400

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>🏴‍☠️ Little Pirate Home Daycare</h1>
+      <p>Welcome aboard! This is the test page for your daycare app.</p>
+    </div>
+  );
+}
